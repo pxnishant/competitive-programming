ea09ce9cd52b2bb9532e273022daa61ee814c2ba
Nishant Singh <nishantsvis@gmail.com>
2025-01-23 14:32:26 +0530

    intime/outtime/lca problem

---
+++ trees/intime-outtime.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define quick ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define fr(n) for(int i=0;i<n;i++)
+#define f first
+#define s second
+#define endl '\n'
+#define pb push_back
+using ll = long long;
+using namespace std;
+using namespace __gnu_pbds;
+const int MOD = 998244353;
+#define int long long
+const int N = 1e9;
+#define int long long
+
+/************************************ debug ************************************/
+
+template <typename T>
+void printe(const T& var) { cout << var << endl; }
+template <typename T>
+void printa(const vector<T>& arr) { for (const auto& elem : arr) cout << elem << " "; cout << endl; }
+template <typename T>
+void print2d(const vector<vector<T>>& mat) { for (const auto& row : mat) { for (const auto& elem : row) cout << elem << " "; cout << endl; } cout << endl; }
+
+/************************************ functions ************************************/
+
+ll _pow(ll a, ll b, ll m) { ll res = 1; while (b > 0) {if (b & 1) res = (res * a) % m; a = (a * a) % m; b = b >> 1;} return res; }
+ll _ceil(ll a, ll b) { return (a+b-1)/b; }
+ll fact(ll n, ll m) { ll res = 1; for (int i=2; i<=n; i++) {res = (res * i)%m;} return res; }
+//understand comb later
+ll comb(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+int set_bit_count(ll n) { return __builtin_popcountll(n); }
+
+/************************************ data structures ************************************/
+
+// find_by_order, order_of_key
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> o_set; 
+ 
+vector<vector<int>> g;
+int t = 0;
+
+void dfs(int node, int prev, vector<int> & intime, vector<int> & outtime) {
+   t++;
+   intime[node] = t;
+   for (int neighbor : g[node]) {
+      if (neighbor == prev) continue;
+      dfs(neighbor, node, intime, outtime);
+   }
+   t++;
+   outtime[node] = t;
+}
+
+void solve() {
+   int n; cin >> n; g.clear(); g.resize(n+1); 
+
+   fr(n-1) {
+      int u, v; cin >> u >> v;
+      g[u].push_back(v); g[v].push_back(u);
+   }
+
+   vector<int> intime(n+1, 0), outtime(n+1, 0);
+
+   dfs(1, -1, intime, outtime);
+   
+   printa(intime);
+   printa(outtime);
+}
+        
+int32_t main() { 
+      quick;
+      int t; t = 1;
+      while (t--) {
+         solve();
+      }
+}
