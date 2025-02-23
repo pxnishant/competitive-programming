4fb5c7dd87e25d805ace99f1a671e68b4ec8680c
Nishant Singh <nishantsvis@gmail.com>
2025-02-23 12:32:46 +0530

    Added Kruskals for fidning MST

---
+++ graphs/kruskals.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define quick ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define fr(n) for(int i=0;i<n;i++)
+#define int long long
+#define ff first
+#define ss second
+#define endl '\n'
+#define pb push_back
+using ll = long long;
+using namespace std;
+using namespace __gnu_pbds;
+const int M = 998244353;
+ 
+/************************************ debug ************************************/
+ 
+template <typename T>
+void printe(const T& var) { cout << var << endl; }
+template <typename T>
+void printa(const vector<T>& arr) { for (const auto& elem : arr) cout << elem << " "; cout << endl; }
+template <typename T>
+void printa1(const vector<T>& arr) { int i = 0; for (const auto& elem : arr) { i++; if (i==1) continue; cout << elem << " "; } cout << endl; }
+template <typename T>
+void print2d(const vector<vector<T>>& mat) { for (const auto& row : mat) { for (const auto& elem : row) cout << elem << " "; cout << endl; } cout << endl; }
+ 
+/************************************ functions ************************************/
+ 
+ll _pow(ll a, ll b, ll m) { ll res = 1; while (b > 0) {if (b & 1) res = (res * a) % m; a = (a * a) % m; b = b >> 1;} return res; }
+ll _ceil(ll a, ll b) { return (a+b-1)/b; }
+ll fact(ll n, ll m) { ll res = 1; for (int i=2; i<=n; i++) {res = (res * i)%m;} return res; }
+//understand comb later
+ll comb(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+int set_bit_count(ll n) { return __builtin_popcountll(n); }
+ 
+/************************************ data structures ************************************/
+ 
+// find_by_order, order_of_key
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> o_set;
+const int N = 1e5;
+
+struct DSU {
+    vector<int> parent, sz;
+    
+    DSU(int n) {
+      parent = vector<int>(n+1);
+      sz = vector<int>(n+1);
+      for (int i = 1; i <= n; i++) parent[i] = i;
+    }
+    
+    int find(int u) {
+        if (u == parent[u]) return u;
+        return parent[u] = find(parent[u]);
+    }
+    
+    void unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            if (sz[a] > sz[b]) swap(a, b);
+            parent[a] = b;
+            sz[b] += sz[a];
+        }
+    }
+};
+
+
+void solve() {
+   while (true) {
+      int n, m; cin >> n >> m;
+      if (n == 0 && m == 0) break;
+
+      DSU dsu(n); int total = 0;
+      vector<vector<int>> edges;
+      while (m--) {
+         int u, v, w; cin >> u >> v >> w;
+         edges.push_back({w, u, v});
+         total += w;
+      }
+
+      sort(edges.begin(), edges.end());
+      int cost = 0;
+
+      for (auto i : edges) {
+         int a = dsu.find(i[1]);
+         int b = dsu.find(i[2]);
+         if (a == b) continue;
+         dsu.unite(a, b);
+         cost += i[0];
+      }
+
+      cout << total - cost << endl;     
+
+   }
+
+}
+
+int32_t main() { 
+      quick;
+      solve();
+}
